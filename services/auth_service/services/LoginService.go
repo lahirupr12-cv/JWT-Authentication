9e76db83dac08eb0bdf5cf6c8af3d76ffc45d596
lahirupr12-cv <lahirub@conceptvines.com>
2023-01-17 08:35:01 +0530

    chnges

--- services/auth_service/services/LoginService.go
+++ services/auth_service/services/LoginService.go
@@ -1,1 +1,20 @@
 package services
+
+type LoginService interface {
+	LogInUser(email string, password string) bool
+}
+
+type LoginInfo struct {
+	email    string
+	password string
+}
+
+func StaticLoginService() LoginService {
+	return &LoginInfo{
+		email:    "lahirupr471@gmail.com",
+		password: "lahiru12@",
+	}
+}
+func (info *LoginInfo) LogInUser(email string, password string) bool {
+	return info.email == email && info.password == password
+}
