8cc49a2c26663b30a3e2d3836d451cd1ee725f7d
lahirupr12-cv <lahirub@conceptvines.com>
2023-01-17 12:31:38 +0530

    completed middleware part

--- services/auth_service/middleware/JWTMiddleware.go
+++ services/auth_service/middleware/JWTMiddleware.go
@@ -1,1 +1,26 @@
 package middleware
+
+import (
+	"fmt"
+	"lahiruprasad12/services/auth_service/services"
+	"net/http"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
+)
+
+func AutherizeJWT() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		const BEARER_SCHEMA = "Bearer"
+		authHeader := c.GetHeader("Authorization")
+		tokenString := authHeader[len(BEARER_SCHEMA):]
+		token, err := services.JWTAuthService().ValidateToken(tokenString)
+		if token.Valid {
+			claims := token.Claims.(jwt.MapClaims)
+			fmt.Println(claims)
+		} else {
+			fmt.Println(err)
+			c.AbortWithStatus(http.StatusUnauthorized)
+		}
+	}
+}
